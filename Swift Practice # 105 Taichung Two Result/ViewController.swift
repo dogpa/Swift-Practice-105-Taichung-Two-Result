2a2a74853080e2fa7c79b00ef7076713b4fc330a
Dogpa <dogpa0130@gmail.com>
2021-11-01 14:33:42 +0800

    Initial Commit

---
+++ Swift Practice # 105 Taichung Two Result/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 105 Taichung Two Result
+//
+//  Created by Dogpa's MBAir M1 on 2021/11/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
